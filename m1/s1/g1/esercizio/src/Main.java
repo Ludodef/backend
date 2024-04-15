7f2390d4a17d41c97f4f83636ebaf1d49bf247e1
Ludodef <defazioludovica95@gmai.com>
2024-04-15 16:45:09 +0200

    Stringa

--- m1/s1/g1/esercizio/src/Main.java
+++ m1/s1/g1/esercizio/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -9,12 +11,31 @@ public class Main {
 
            System.out.println(concat("ciao ", 4));
 
+        String[] arr = insertIntoArray(new String[]{"l", "u", "d", "o", "v"}, "icaa");
+
+        System.out.println(Arrays.toString(arr));
+
     }
     public static int multiply(int a, int b) {
         return a * b;
     }
     public static String concat( String str, int num){
         return str + num;
+    }
+
+    public static String[] insertIntoArray(String[] stringArray, String str) {
+        String[] array = new String[6];
+        for (int i = 0; i < stringArray.length +1 ; i++) {
+            if(i < 2) {
+                array[i] = stringArray[i];
+            } else if(i == 2) {
+                array[i] = str;
+            } else {
+                array[i] = stringArray[i - 1];
+            }
+        }
+
+        return array;
     }
 
 
