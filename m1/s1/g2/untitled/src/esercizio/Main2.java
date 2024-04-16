50710ee19e8f2379ea64422f42bef526a9ed93e3
Ludodef <defazioludovica95@gmai.com>
2024-04-16 16:35:46 +0200

    Esercizio dgff

--- m1/s1/g2/untitled/src/esercizio/Main2.java
+++ m1/s1/g2/untitled/src/esercizio/Main2.java
@@ -1,4 +1,29 @@
 package esercizio;
 
 public class Main2 {
+
+    public static String stampaNumero(int num){
+        String risultato;
+        switch (num){
+
+            case 0:{
+                risultato = "zero";
+                break;
+            }
+            case 1:{
+                risultato = "uno";
+                break;
+            }
+            case 2:{
+                risultato = "due";
+                break;
+            }
+            case 3:{
+                risultato = "tre";
+                break;
+            }
+            default:
+                risultato = "ERRORE";
+        }
+    }
 }
