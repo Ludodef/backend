85ff77ff4fd8b4fd00685d4449dc841e6cd9aa7c
Ludodef <defazioludovica95@gmai.com>
2024-05-17 10:41:58 +0200

    inizializzazione progetto

---
+++ m2/s1/g5/verifica-venerdi/src/main/java/it/epicode/verificavenerdi/VerificaVenerdiApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.verificavenerdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificaVenerdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerificaVenerdiApplication.class, args);
+	}
+
+}
