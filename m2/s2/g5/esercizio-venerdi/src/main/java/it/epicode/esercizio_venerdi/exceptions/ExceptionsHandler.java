bf6fcd3f3a8408f1812ec1c31767853857d35f95
Ludodef <defazioludovica95@gmai.com>
2024-05-24 17:48:38 +0200

    exception

--- m2/s2/g5/esercizio-venerdi/src/main/java/it/epicode/esercizio_venerdi/exceptions/ExceptionsHandler.java
+++ m2/s2/g5/esercizio-venerdi/src/main/java/it/epicode/esercizio_venerdi/exceptions/ExceptionsHandler.java
@@ -1,6 +1,6 @@
 package it.epicode.esercizio_venerdi.exceptions;
 
-import epicode.u5d9hw.payloads.errors.ErrorsPayloadWithList;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
